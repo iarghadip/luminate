53f1910a0b7c8c3c94c48d3ab4195921d45d808b
Arghadip Das <72627807+iarghadip@users.noreply.github.com>
2025-06-23 21:16:53 +0530

    updated app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -75,7 +75,7 @@ void setup() {
     _led.begin(&_mcu);
     _rtc.begin(&_mcu);
     _mcu.updateLogginTimestamp(_rtc.read());
-    _mcu.log("setup(): Welcome to " + String(FIRMWARE_NAME) + " (v" + String(FIRMWARE_VERSION) + ").");
+    _mcu.log("setup(): Welcome to " + String(FIRMWARE_NAME) + " (" + String(FIRMWARE_VERSION) + ").");
     _mcu.assign(1, updateWSL);
     _mcu.assign(1, updateLED);
     _mcu.assign(1, updateRTC);
